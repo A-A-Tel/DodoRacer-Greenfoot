ed003d3e8f66f9c5c69f42bd0126791e31eb3f75
a-a-tel <anouaranthonytel@gmail.com>
2025-05-13 12:51:11 +0200

    init

---
+++ MyDodo.java
@@ -0,0 +1,130 @@
+import greenfoot.World;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sjaak Smetsers & Renske Smetsers-Weeda
+ * @version 3.1 -- 29-07-2017
+ */
+public class MyDodo extends Dodo
+{
+    /* ATTRIBUTE DECLARATIONS: */
+    private int numOfSteps;
+           
+    public MyDodo() {
+        super( EAST );
+        /* INITIALISATION OF ATTRIBUTES: */
+        numOfSteps = 0;
+    }
+
+    /* METHODS OF THE CLASS: */
+
+    public void act() {
+    }
+
+    /**
+     * Move one cell forward in the current direction.
+     * 
+     * <P> Initial: Dodo is somewhere in the world
+     * <P> Final: If possible, Dodo has moved forward one cell
+     *
+     */
+    public void move() {
+        if ( canMove() ) {
+            step();
+            numOfSteps++;
+        } else {
+            showError( "I'm stuck!" );
+        }
+    }
+
+
+
+    public void collectAllEggs() {
+        setLocation(0, 0);
+        while (!borderUnder()) {
+
+        }
+    }
+
+    /**
+     * Test if Dodo can move forward, 
+     * i.e. there are no obstructions or end of world in the cell in front of her.
+     * 
+     * <p> Initial:   Dodo is somewhere in the world
+     * <p> Final:     Same as initial situation
+     * 
+     * @return  boolean true if Dodo can move (thus, no obstructions ahead)
+     *                  false if Dodo can't move
+     *                      there is an obstruction or end of world ahead
+     */
+    public boolean canMove() {
+        if ( borderAhead() || fenceAhead() ){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Move given number of cells forward in the current direction.
+     * 
+     * <p> Initial:   
+     * <p> Final:  
+     * <p>
+     * param int distance: the number of steps made
+     */
+    public void jump( int distance ) {
+        int nrStepsTaken = 0;               // set counter to 0
+        while ( nrStepsTaken < distance ) { // check if more steps must be taken  
+            move();                         // take a step
+            nrStepsTaken++;                 // increment the counter
+        }
+    }
+
+    /**
+    * Places all the Egg objects in the world in a list.
+    * 
+    * @return List of Egg objects in the world
+    */
+    public List<Egg> getListOfEggsInWorld() {
+        return getWorld().getObjects(Egg.class);
+    }
+    
+    public List<Integer> createListOfNumbers() {
+        return new ArrayList<> (Arrays.asList( 2, 43, 7, -5, 12, 7 ));
+    }
+    
+
+    /**
+     * Method for praciticing with lists.
+     */
+    public void practiceWithLists( ){
+        List<Integer> listOfNumbers = createListOfNumbers();
+        System.out.println("First element: " + listOfNumbers.get(1) ); 
+    }
+
+    public void practiceWithListsOfSurpriseEgss( ){
+        List<SurpriseEgg>  listOfEgss = SurpriseEgg.generateListOfSurpriseEggs( 12, getWorld() );
+    }
+
+    private boolean borderUnder() {
+        World world = getWorld();
+        int height = world.getHeight();
+        return getY() == height - 1;
+    }
+
+    private void moveDown() {
+        turnLeft();
+        move();
+        turnRight();
+    }
+
+    private void turn180() {
+        turnRight();
+        turnRight();
+    }
+}
